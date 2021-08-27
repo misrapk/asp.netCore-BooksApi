52056803d1f0a3dffa8bc8d07d684e6fdf63613c
Kant Misra <935345@cognizant.com>
2021-08-27 11:49:44 +0530

    Added Get,Post, Put and delete

---
+++ my-books/Controllers/BooksController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using my_books.Data.Services;
+using my_books.Data.ViewModels;
+
+namespace my_books.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BooksController : ControllerBase
+	{
+		public BooksService _bookService;
+		public BooksController(BooksService booksService)
+		{
+			_bookService = booksService;
+		}
+
+		[HttpGet("get-all-books")]
+		public IActionResult GetAllBooks()
+		{
+			var allBooks = _bookService.GetAllBooks();
+			return Ok(allBooks);
+		}
+
+		[HttpGet("get-book-by-id/{id}")]
+		public IActionResult GetBookbyId(int id)
+		{
+			var singleBook = _bookService.GetBookbyId(id);
+			return Ok(singleBook);
+		}
+
+		[HttpPost("add-book")]
+		public IActionResult AddBook([FromBody] BookVM book)
+		{
+			_bookService.AddBook(book);
+			return Ok();
+		}
+
+		[HttpPut("update-book-by-id/{id}")]
+		public IActionResult UpdaeBookById(int id, [FromBody] BookVM book)
+		{
+			var updatedBook = _bookService.UpdateBookById(id, book);
+			return Ok(updatedBook);
+		}
+		[HttpDelete("delete-book-by-id/{id}")]
+		public IActionResult DeleteBookbyId(int id)
+		{
+			_bookService.DeleteBookbyId(id);
+			return Ok();
+		}
+	}
+}
---
+++ my-books/Data/AppDbInitialiser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using my_books.Data.Models;
+
+namespace my_books.Data
+{
+	public class AppDbInitialiser
+	{
+		public static void Seed(IApplicationBuilder applicaitonBuilder)
+		{
+			using (var serviceScope = applicaitonBuilder.ApplicationServices.CreateScope())
+			{
+				var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+				if (!context.Books.Any())
+				{
+					context.Books.AddRange(new Book()
+					{
+						Title = "1stBook",
+						Description = "1st description",
+						IsRead = true,
+						DateRead = DateTime.Now.AddDays(-10),
+						Rate = 400,
+						Genre = "Biography",
+						CoverUrl = "test.com",
+						DateAdded = DateTime.Now
+					},
+					new Book()
+					{
+						Title = "2ndBook",
+						Description = "2nd description",
+						IsRead = true,
+						DateRead = DateTime.Now.AddDays(-20),
+						Rate = 500,
+						Genre = "Story",
+						CoverUrl = "test.com",
+						DateAdded = DateTime.Now
+					});
+					context.SaveChanges();
+				}
+
+			}
+		}
+
+	}
+}
